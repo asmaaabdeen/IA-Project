9092c26a9e676ad7372950c0a91f8f981e850f68
asmaaabdeen <62287975+asmaaabdeen@users.noreply.github.com>
2020-04-07 10:16:40 +0200

    Add files via upload

---
+++ Controllers/ProductController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using try_search.Models;
+
+namespace try_search.Controllers
+{
+    public class ProductController : Controller
+    {
+        // GET: Product
+        
+        public ActionResult Index(string search)
+        {
+
+            emarketEntities sd = new emarketEntities();
+            List<pProduct> products = sd.pProducts.ToList();
+            return View(sd.pProducts.Where(x=>x.cCategory.name.StartsWith(search)|| search== null).ToList());
+        }
+    }
+}
